a3b877bb9a47f77357a298b4aece3e2baa45499a
arbolitoloco1 <santiago@arbolitoloco.xyz>
2026-07-27 08:32:15 -0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mwrogue",
-    version="0.1.7",
+    version="0.1.8",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="Client for accessing Fandom/Gamepedia Esports Wikis",
